6e58d4bcf3c0b05386f01de9940c47e5bd87fe30
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-28 17:09:49 +0530

    [mrunal] Add. Created classes and added welcome message

---
+++ IndianStatesCensusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,22 @@
+﻿using IndianStatesCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US , BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
